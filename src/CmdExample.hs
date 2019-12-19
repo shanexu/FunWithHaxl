647a750f6f6aa060844b03147ccd239eaa7ea772
Xu Shane <xusheng0711@gmail.com>
2019-12-19 16:04:35 +0800

    cmd example

---
+++ src/CmdExample.hs
@@ -0,0 +1,13 @@
+module CmdExample where
+
+import CmdDataSource
+import Haxl.Core
+import LogDataSource
+
+type Haxl a = GenHaxl () a
+
+run :: Haxl a -> IO a
+run h = do
+  st <- CmdDataSource.initDataSource
+  env <- initEnv (stateSet st stateEmpty ) ()
+  runHaxl env h
